014b3ba94bb6029e6045ee9b664aff90ee0bcb36
Raptoer <Raptoer@gmail.com>
2016-10-16 22:11:00 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,9 @@ from distutils.core import setup
 setup(
     name='FAMDB',
     version='2',
-    package_dir={'': ''},
     url='https://github.com/Raptoer/FAMDB',
     license='',
     author='abarton',
     author_email='Raptoer@gmail.com',
-    description='Arma mission database',
-    packages=[
-        'passlib',
-        'pycrypto',
-        'psutil'
-    ]
+    description='Arma mission database'
 )
