878ea6fefe1eb81d8dc8a921e003e1bf63427399
Raptoer <Raptoer@gmail.com>
2016-10-16 22:33:17 -0700

    uping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='FAMDB',
-    version='2',
+    version='2.0.1-alpha',
     url='https://github.com/Raptoer/FAMDB',
     license='',
     author='abarton',
