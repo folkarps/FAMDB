3fa8de3aa9827c583ec0fd5b6661897ff935a5c2
abarton <abarton@autoreturn.com>
2017-10-28 09:58:04 -0700

    Fixing cleanup

--- cleanup.py
+++ cleanup.py
@@ -1,6 +1,4 @@
-import gzip
 import os
-import shutil
 
 import utils
 
@@ -15,25 +13,13 @@ def handleCleanup(environ, start_response):
     for origin in ['main', 'missionMaker']:
         if origin == 'main':
             missionDirPrefix = utils.missionMainDir
-            missionArchivePrefix = utils.missionMainArchive
-            toBeArchivedProperty = 'toBeArchivedMain'
             toBeDeletedProperty = 'toBeDeletedMain'
             existsProperty = 'existsOnMain'
         else:
             missionDirPrefix = utils.missionMakerDir
-            missionArchivePrefix = utils.missionMakerArchive
-            toBeArchivedProperty = 'toBeArchivedMM'
             toBeDeletedProperty = 'toBeDeletedMM'
             existsProperty = 'existsOnMM'
 
-        c.execute('''select * from versions where ''' + toBeArchivedProperty + ''' = 1''')
-        toBeArchived = c.fetchall()
-
-        for forArchival in toBeArchived:
-            with open(missionDirPrefix + "/" + forArchival['name'], 'rb') as f_in:
-                with gzip.open(os.path.join(missionArchivePrefix, forArchival['name'] + ".gz"), 'wb') as f_out:
-                    shutil.copyfileobj(f_in, f_out)
-                    os.remove(f_in.name)
 
         c.execute('''select * from versions where ''' + toBeDeletedProperty + ''' = 1''')
         toBeDeleted = c.fetchall()
@@ -44,8 +30,8 @@ def handleCleanup(environ, start_response):
                 pass
             
         c.execute(
-            "update versions set " + existsProperty + " = 0, " + toBeArchivedProperty + " = 0, " + toBeDeletedProperty + " = 0 where " +
-            toBeArchivedProperty + " = 1 or " + toBeDeletedProperty + " = 1")
+            "update versions set " + existsProperty + " = 0, " + toBeDeletedProperty + " = 0 where " +
+            + toBeDeletedProperty + " = 1")
     c.execute("delete from versions where existsOnMM = 0 and existsOnMain = 0")
     c.connection.commit()
     c.connection.close()
