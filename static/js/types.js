c68d91225ee0b3b9521ddb5ded6a335fd2d9110f
Euan Hunter <SamLex@users.noreply.github.com>
2019-05-19 19:04:43 +0100

    Added GM maps (Weferlingen variants)

--- static/js/types.js
+++ static/js/types.js
@@ -1,5 +1,5 @@
 var missionTypes = ["Adversarial","Coop","Coop (ZEUS)","Afterparty", "Race"];
-var islands = ["Altis","Tanoa","Stratis","Malden","Virtual Reality"];
+var islands = ["Altis","Malden","Stratis","Tanoa","Weferlingen (Summer)","Weferlingen (Winter)","Virtual Reality"];
 var statuses = ["Broken", "WIP", "Ready", "Testing", "Transfer", "Testing & Transfer"];
 
 
